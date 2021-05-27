f68ffaf909639002c0d9e7a79be5278b5f8b2078
Rajiv <r.sarkar3269@gmail.com>
2021-05-28 00:25:25 +0530

    second commit

---
+++ ipl-dashboard/src/main/java/com/fullstack/ipldashboard/entity/Match.java
@@ -0,0 +1,31 @@
+package com.fullstack.ipldashboard.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Match {
+
+    @Id
+    private Long id;
+    private String city;
+    private LocalDate date;
+    private String playerOfMatch;
+    private String venue;
+    private String team1;
+    private String team2;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
+    private String result;
+    private String resultMargin;
+    private String umpire1;
+    private String umpire2;
+}
