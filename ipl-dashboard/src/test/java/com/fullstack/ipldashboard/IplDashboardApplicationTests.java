48d442cfff33df031b7a40fe5fbc7ae8e159e5fa
Rajiv <r.sarkar3269@gmail.com>
2021-05-27 13:10:35 +0530

    first commit project skeleton

---
+++ ipl-dashboard/src/test/java/com/fullstack/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fullstack.ipldashboard;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		assertNotNull("String");
+	}
+
+}
